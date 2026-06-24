e4aebb6ceb7c3c4bcb672055ea252b10d94e1274
Dacbconst <dconstante@xplora.net>
2026-06-24 10:05:32 -0500

    discrad3

--- Proyectos/Pintuco/partials/tab-fotografico.php
+++
@@ -1,7 +0,0 @@
-<div id="cards-container"
-     style="background:#fff; border-radius:6px 6px 0 0;
-            padding:12px 16px; box-shadow:0 1px 6px rgba(0,0,0,.08);">
-    <table style="width:100%">
-        <tbody id="data" name="data"></tbody>
-    </table>
-</div>
